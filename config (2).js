cbe2247f2d8396f7253942f642027b11e2b022cb
Imran-Ik <imranik2828@gmail.com>
2026-09-08 19:51:37 +0530

    Delete config (2).js

--- config (2).js
+++
@@ -1,7 +0,0 @@
-// ---- EDIT THESE TWO VALUES AFTER YOU DEPLOY THE APPS SCRIPT BACKEND ----
-
-// Paste your Apps Script Web App URL here (ends in /exec)
-const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbwaeL0pt11CEUmgOrDxUpXNGzbR1buuUeaujLB1vVDGNBQRtusBTvay9OecUnftIvkI/exec';
-
-// Must exactly match the API_KEY constant in Code.gs
-const API_KEY = 'Ik_181001_@67890';
