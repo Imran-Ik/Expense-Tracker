747acc059d86e371fa40bb4fdb8dbf7de5780a9b
Imran-Ik <imranik2828@gmail.com>
2026-09-08 20:02:26 +0530

    new Deploy.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 // ---- EDIT THESE TWO VALUES AFTER YOU DEPLOY THE APPS SCRIPT BACKEND ----
 
 // Paste your Apps Script Web App URL here (ends in /exec)
-const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbwaeL0pt11CEUmgOrDxUpXNGzbR1buuUeaujLB1vVDGNBQRtusBTvay9OecUnftIvkI/exec';
+const BACKEND_URL = 'https://script.google.com/macros/s/AKfycbwtvWm53FMwtb9oR8xdsN-X7GoF8yzxKMis3l1OfThQz93koONNl9-jdKp5eqCyXtu4/exec';
 
 // Must exactly match the API_KEY constant in Code.gs
 const API_KEY = 'Ik_181001_@67890';
